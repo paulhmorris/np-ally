4f1da4382b55cd1dd717327af2a2411388a1c20a
paulhmorris <paulh.morris@gmail.com>
2023-12-04 15:53:28 -0600

    feat: user nav dropdown menu

--- app/components/desktop-nav.tsx
+++ app/components/desktop-nav.tsx
@@ -1,11 +1,20 @@
 import { Form, Link, NavLink, useNavigation } from "@remix-run/react";
-import { IconLoader, IconWorld } from "@tabler/icons-react";
+import { IconLoader, IconMoon, IconSun, IconWorld } from "@tabler/icons-react";
 import type { ComponentPropsWithoutRef } from "react";
+import { Theme, useTheme } from "remix-themes";
 import { useSpinDelay } from "spin-delay";
 
-import { ThemeModeToggle } from "~/components/theme-mode-toggle";
 import { Avatar, AvatarFallback } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 import { Separator } from "~/components/ui/separator";
 import { navLinks } from "~/lib/constants";
 import { cn, useUser } from "~/lib/utils";
@@ -14,6 +23,12 @@ export function DesktopNav(props: ComponentPropsWithoutRef<"nav">) {
   const user = useUser();
   const navigation = useNavigation();
   const showSpinner = useSpinDelay(navigation.state !== "idle");
+  const [_, setTheme] = useTheme();
+
+  function handleToggleTheme(e: Event) {
+    e.preventDefault();
+    setTheme((theme) => (theme === Theme.DARK ? Theme.LIGHT : Theme.DARK));
+  }
 
   return (
     <nav
@@ -70,31 +85,46 @@ export function DesktopNav(props: ComponentPropsWithoutRef<"nav">) {
           </NavLink>
         </>
       ) : null}
-      <div className="mt-auto space-y-4">
-        <Link to={`/users/${user.id}`} className="flex gap-2">
-          <Avatar>
-            <AvatarFallback className="bg-primary text-white">
-              {user.contact.firstName?.charAt(0).toUpperCase()}
-              {user.contact.lastName?.charAt(0).toUpperCase()}
-            </AvatarFallback>
-          </Avatar>
-          <div>
-            <div className="text-sm font-medium text-secondary-foreground">
-              {user.contact.firstName}
-              {user.contact.lastName ? ` ${user.contact.lastName}` : null}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="relative mt-auto h-10 w-10 rounded-full">
+            <Avatar>
+              <AvatarFallback className="bg-primary text-white">
+                {user.contact.firstName?.charAt(0).toUpperCase()}
+                {user.contact.lastName?.charAt(0).toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="mb-2 w-48" align="start" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {user.contact.firstName}
+                {user.contact.lastName ? ` ${user.contact.lastName}` : null}
+              </p>
+              <p className="text-xs leading-none text-muted-foreground">{user.contact.email}</p>
             </div>
-            <div className="max-w-[150px] truncate text-xs text-muted-foreground">{user.contact.email}</div>
-          </div>
-        </Link>
-        <div className="flex items-center gap-2">
-          <Form method="post" action="/logout" navigate={false}>
-            <Button type="submit" variant="outline">
-              Log out
-            </Button>
-          </Form>
-          <ThemeModeToggle />
-        </div>
-      </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem asChild>
+              <Link to={`/users/${user.id}`}>Profile</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem onSelect={handleToggleTheme}>
+              <span>Toggle theme</span>
+              <IconSun className="absolute right-2 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <IconMoon className="absolute right-2 h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <Form method="post" action="/logout" navigate={false}>
+              <button>Log out</button>
+            </Form>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </nav>
   );
 }
