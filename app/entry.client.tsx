c78b6f352c3a825aeafac3d0a29f34a57985cf06
paulhmorris <paulh.morris@gmail.com>
2024-01-10 12:11:07 -0600

    fix: lower sentry replay rate

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -11,7 +11,7 @@ Sentry.init({
       ? undefined
       : "https://f18051d71458f411f51af7ca0308b1cb@o4505496663359488.ingest.sentry.io/4506395673886720",
   tracesSampleRate: 0.25,
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0.01,
   replaysOnErrorSampleRate: 1,
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   environment: window?.ENV?.VERCEL_ENV,
