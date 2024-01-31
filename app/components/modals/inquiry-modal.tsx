e74fbd928a0b0ade866c26c58de5d68d793acb60
paulhmorris <paulh.morris@gmail.com>
2024-01-31 16:46:12 -0600

    feat: inquiries

---
+++ app/components/modals/inquiry-modal.tsx
@@ -0,0 +1,77 @@
+import { useFetcher } from "@remix-run/react";
+import { IconLoader } from "@tabler/icons-react";
+import { useEffect } from "react";
+import { ValidatedForm } from "remix-validated-form";
+
+import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "~/components/ui/dialog";
+import { FormField, FormSelect, FormTextarea } from "~/components/ui/form";
+import { SelectItem } from "~/components/ui/select";
+import { useUser } from "~/lib/utils";
+import { validator } from "~/routes/api.inquiries";
+
+export function NewInquiryModal({ open, onOpenChange }: { open: boolean; onOpenChange: (open: boolean) => void }) {
+  const user = useUser();
+  const fetcher = useFetcher<{ success: boolean }>();
+  const isSubmitting = fetcher.state !== "idle";
+
+  useEffect(() => {
+    if (fetcher.data && !isSubmitting && fetcher.data.success) {
+      onOpenChange(false);
+    }
+  }, [fetcher.data, isSubmitting, onOpenChange]);
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Ask a question</DialogTitle>
+          <DialogDescription>Your message will be sent to an administrator.</DialogDescription>
+        </DialogHeader>
+        <ValidatedForm
+          id="inquiry-form"
+          method="post"
+          action="/api/inquiries"
+          fetcher={fetcher}
+          validator={validator}
+          defaultValues={{
+            name: `${user.contact.firstName} ${user.contact.lastName}`,
+            email: user.username,
+            method: "Email",
+          }}
+        >
+          <div className="space-y-4">
+            <FormField name="name" label="Name" required />
+            <FormSelect name="method" label="Contact Method" placeholder="Select method" required>
+              <SelectItem value="Phone">Phone</SelectItem>
+              <SelectItem value="Email">Email</SelectItem>
+              <SelectItem value="WhatsApp">WhatsApp</SelectItem>
+              <SelectItem value="Signal">Signal</SelectItem>
+              <SelectItem value="Other">Other (please specify)</SelectItem>
+            </FormSelect>
+            <FormField name="otherMethod" label="Other Method" />
+            <FormField name="email" label="Email" />
+            <FormField name="phone" label="Phone" />
+            <FormTextarea name="message" label="Message" maxLength={1000} rows={5} required />
+          </div>
+          <DialogFooter className="mt-4 gap-2 sm:space-x-0">
+            <Button variant="outline" type="submit" onClick={() => onOpenChange(false)} disabled={isSubmitting}>
+              Cancel
+            </Button>
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? <IconLoader className="size-4 animate-spin" /> : null}
+              <span>Submit</span>
+            </Button>
+          </DialogFooter>
+        </ValidatedForm>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- app/components/user-menu.tsx
+++ app/components/user-menu.tsx
@@ -1,6 +1,8 @@
 import { UserRole } from "@prisma/client";
 import { Form, Link } from "@remix-run/react";
+import { useState } from "react";
 
+import { NewInquiryModal } from "~/components/modals/inquiry-modal";
 import { Avatar, AvatarFallback } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
 import {
@@ -16,69 +18,70 @@ import { useUser } from "~/lib/utils";
 
 export function UserMenu() {
   const user = useUser();
-  // const [_, setTheme] = useTheme();
-
-  // function handleToggleTheme(e: Event) {
-  //   e.preventDefault();
-  //   setTheme((theme) => (theme === Theme.DARK ? Theme.LIGHT : Theme.DARK));
-  // }
+  const [inquiryOpen, setInquiryOpen] = useState(false);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative mt-auto h-10 w-10 rounded-full">
-          <span className="sr-only">Open User Menu</span>
-          <Avatar aria-hidden="true">
-            <AvatarFallback className="bg-primary text-white" aria-hidden="true">
-              {user.contact.firstName?.charAt(0).toUpperCase()}
-              {user.contact.lastName?.charAt(0).toUpperCase()}
-            </AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="mb-2 w-48" align="start" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-2 sm:space-y-1">
-            <p className="text-sm font-medium leading-none">
-              {user.contact.firstName}
-              {user.contact.lastName ? ` ${user.contact.lastName}` : null}
-            </p>
-            <p className="text-xs leading-none text-muted-foreground">{user.contact.email}</p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem asChild>
-            <Link
-              className="cursor-pointer sm:hidden"
-              to={user.role === UserRole.USER ? "/dashboards/staff" : "/dashboards/admin"}
-            >
-              Home
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild>
-            <Link className="cursor-pointer" to={`/users/${user.id}`}>
-              Profile
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild>
-            <Link className="cursor-pointer" to="/feature-request">
-              Feature Request
-            </Link>
-          </DropdownMenuItem>
-          {/* <DropdownMenuItem className="cursor-pointer" onSelect={handleToggleTheme}>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="relative mt-auto h-10 w-10 rounded-full">
+            <span className="sr-only">Open User Menu</span>
+            <Avatar aria-hidden="true">
+              <AvatarFallback className="bg-primary text-white" aria-hidden="true">
+                {user.contact.firstName?.charAt(0).toUpperCase()}
+                {user.contact.lastName?.charAt(0).toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="mb-2 w-48" align="start" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-2 sm:space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {user.contact.firstName}
+                {user.contact.lastName ? ` ${user.contact.lastName}` : null}
+              </p>
+              <p className="text-xs leading-none text-muted-foreground">{user.contact.email}</p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem asChild>
+              <Link
+                className="cursor-pointer sm:hidden"
+                to={user.role === UserRole.USER ? "/dashboards/staff" : "/dashboards/admin"}
+              >
+                Home
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link className="cursor-pointer" to={`/users/${user.id}`}>
+                Profile
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link className="cursor-pointer" to="/feature-request">
+                Feature Request
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" onClick={() => setInquiryOpen(true)}>
+              New Inquiry
+            </DropdownMenuItem>
+            {/* <DropdownMenuItem className="cursor-pointer" onSelect={handleToggleTheme}>
             <span>Toggle theme</span>
             <IconSun className="absolute right-2 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <IconMoon className="absolute right-2 h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           </DropdownMenuItem> */}
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem className="px-0 py-0">
-          <Form className="w-full" method="post" action="/logout" navigate={false}>
-            <button className="w-full px-2 py-1.5 text-left">Log out</button>
-          </Form>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="px-0 py-0">
+            <Form className="w-full" method="post" action="/logout" navigate={false}>
+              <button className="w-full px-2 py-1.5 text-left">Log out</button>
+            </Form>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <NewInquiryModal open={inquiryOpen} onOpenChange={setInquiryOpen} />
+    </>
   );
 }
