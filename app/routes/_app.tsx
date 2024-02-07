0ea2e8a2044d1f1f0d69169c4bf0bdd7c6d6c6c6
paulhmorris <paulh.morris@gmail.com>
2024-02-07 13:22:17 -0600

    style: work on drawer blackness

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -6,7 +6,7 @@ import { MobileNav } from "~/components/mobile-nav";
 export default function AppLayout() {
   return (
     // eslint-disable-next-line react/no-unknown-property
-    <div vaul-drawer-wrapper="" className="mx-auto flex h-full min-h-dvh w-full flex-col bg-background md:flex-row">
+    <div vaul-drawer-wrapper="" className="mx-auto flex min-h-dvh w-full flex-col bg-background md:flex-row">
       <MobileNav />
       <DesktopNav />
       <main className="w-full max-w-screen-2xl grow p-6 md:ml-64 md:p-10">
