05518d159b79e74ebaae1afe920691386ad864e3
paulhmorris <paulh.morris@gmail.com>
2024-04-06 22:25:22 -0500

    fix: favicon

--- app/root.tsx
+++ app/root.tsx
@@ -67,10 +67,7 @@ function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#1a1a1a" />
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🌐</text></svg>"
-        />
+        <link rel="icon" href="https://fav.farm/🌐" />
         <Meta />
         <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
